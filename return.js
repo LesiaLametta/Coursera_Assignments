e729d6bcc70f26ff33a7c30e966a28c9642c4036
Lesia Lametta <lesialametta@gmail.com>
2017-11-19 19:35:37 +0300

    Return
    To return a result, we can use the return keyword.

---
+++ return.js
@@ -0,0 +1,21 @@
+let orderCount = 0;
+const takeOrder = (topping, crustType) => {
+    orderCount++;
+    console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+
+const getSubTotal = (itemCount) => {
+    return itemCount * 7.5;
+}
+
+console.log(getSubTotal(orderCount));
+
+//output: Order: thin crust pizza topped with mushroom
+//		  Order: whole wheat pizza topped with spinach
+//		  Order: brooklyn style pizza topped with pepperoni
+//		  22.5
