7d698df2d18b39569029fe692d1c67fe26dbc3a7
Lesia Lametta <lesialametta@gmail.com>
2017-11-19 21:42:34 +0300

    Function Declarations sample code
    A function declaration is a function that is bound to an identifier or name.

---
+++ function_declarations.js
@@ -0,0 +1,19 @@
+function isGreaterThan(numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+isGreaterThan(19, 8)]
+
+//Output: true
+
+
+function square (number) {
+  return number * number; 
+}
+
+console.log(square(5));
+// Output: 25. 
