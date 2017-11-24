432d5a10629c263ad4b9af355978cf7a68651fe6
Lesia Lametta <lesialametta@gmail.com>
2017-11-24 22:07:37 +0300

    arrays sample simple code

---
+++ arrays.js
@@ -0,0 +1,6 @@
+let newYearsResolutions = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
+
+
+let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(bucketList);
