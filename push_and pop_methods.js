d2927e3330812a4734b44da4788c02e067543af9
Lesia Lametta <lesialametta@gmail.com>
2017-11-25 12:37:56 +0300

    Push() and Pop() methods Sample Code

---
+++ push_and pop_methods.js
@@ -0,0 +1,9 @@
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+newYearsResolutions.push ('Become a cool Frontend Developer', 'Go to the gym and enjoy');
+
+console.log(newYearsResolutions);
+
+newYearsResolutions.pop();
+
+console.log(newYearsResolutions);
+
