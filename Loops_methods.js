b7c9670eb9da3e60c79b8b83f19e01ad11a17c70
Lesia Lametta <lesialametta@gmail.com>
2017-11-26 20:32:19 +0300

    loops code sample

---
+++ Loops_methods.js
@@ -0,0 +1,11 @@
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+
+for (let i = 0; i<cookies.length; i++) {
+  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
+
+/*I would love to eat a chocolate chip cookie right now!
+I would love to eat a raisin cookie right now!
+I would love to eat a macadamia nut cookie right now!
+I would love to eat a sugar cookie right now!
+*/
